68f8ce42c25696e312d87ebd4b0acbff4718016f
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-16 13:04:46 +0300

    added function to save product

--- dukaapp/models.py
+++ dukaapp/models.py
@@ -12,16 +12,29 @@ class Category(models.Model):
     sub_category = models.ForeignKey("Sub-Category", on_delete=models.CASCADE, related_name='category')
     image = CloudinaryField('image')
 
+    def __str__(self):
+        return self.name
+
 class Sub-Category(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name='sub-category')
 
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     item_name = models.CharField(max_length=100)
     description = models.TextField()
-    price = models.DecimalField(max_digits=1000,decimal_places=2)
+    price = models.IntegerField()
     date_added = models.DateTimeField(auto_now_add=True)
     image = CloudinaryField('image')
     comment = models.ForeignKey("Comment", on_delete=models.CASCADE, related_name='product')
+
+
+    def __str__(self):
+        return self.item_name
+
+    def save_product(self):
+        self.save()
