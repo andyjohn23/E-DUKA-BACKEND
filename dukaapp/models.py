f014892fcaab3d7c3ae4cbcad7ca011b9dd31705
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-16 11:22:37 +0300

    created shop model that will store shop details

--- dukaapp/models.py
+++ dukaapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Shop(models.Model):
+    merchant_name = models.CharField(max_length=100)
+    business_description = models.TextField()
+    date_started = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name='category')
