96bd7d550669410139e9a474abd46eafe86d98ed
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-16 11:40:18 +0300

    created product model to store products details

--- dukaapp/models.py
+++ dukaapp/models.py
@@ -17,3 +17,11 @@ class Sub-Category(models.Model):
     description = models.TextField()
     product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name='sub-category')
 
+
+class Product(models.Model):
+    item_name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=1000,decimal_places=2)
+    date_added = models.DateTimeField(auto_now_add=True)
+    image = CloudinaryField('image')
+    comment = models.ForeignKey("Comment", on_delete=models.CASCADE, related_name='product')
