c77145798fcb727db420d7c325beaa46ac866a2b
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-12-16 11:35:35 +0300

    created the sub category model

--- dukaapp/models.py
+++ dukaapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Shop(models.Model):
     merchant_name = models.CharField(max_length=100)
-    business_description = models.TextField()
+    description = models.TextField()
     date_started = models.DateTimeField(auto_now_add=True)
     category = models.ForeignKey("Category", on_delete=models.CASCADE, related_name='shop')
 
@@ -11,3 +11,9 @@ class Category(models.Model):
     name = models.CharField(max_length=100)
     sub_category = models.ForeignKey("Sub-Category", on_delete=models.CASCADE, related_name='category')
     image = CloudinaryField('image')
+
+class Sub-Category(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, related_name='sub-category')
+
