f126a67547e2d2b3b1269a427f8a537406519f42
Josephshitandi <jsphshtnd@gmail.com>
2020-12-18 06:20:08 +0300

    Logout APIViewuser

--- dukaapp/views.py
+++ dukaapp/views.py
@@ -13,10 +13,30 @@ from django.http import HttpResponse, Http404
 
 
 
-
 class UserViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing user instances.
     """
     serializer_class = UserSignupSerializer
-    queryset = User.objects.a
+    queryset = User.objects.all()
+
+class LogoutAPIView(generics.CreateAPIView):
+    serializer_class=LogoutSerializer
+    permission_classes=(IsAuthenticated,)
+    def post(self,request):
+        serializer=self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token=request.data.get('refresh_token')
+        error_message={
+            "error":"Token is invalid or expired"
+        }
+        success_message={
+            "success":"Logout successfully"
+        }        
+        try:
+            token=RefreshToken(token)
+            token.blacklist()
+        except TokenError as error:
+            return Response(error_message,status=status.HTTP_400_BAD_REQUEST)
+        return Response(success_message,status=status.HTTP_200_OK)
+
