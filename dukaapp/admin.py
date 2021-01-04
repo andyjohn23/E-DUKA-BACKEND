92c89ecebbed538a745f9143868121d4b61ce00d
Josephshitandi <jsphshtnd@gmail.com>
2021-01-04 10:41:10 +0300

    Updates admin.py

--- dukaapp/admin.py
+++ dukaapp/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Shop)
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Sub_Category)
+admin.site.register(Comment)
+admin.site.register(Order)
