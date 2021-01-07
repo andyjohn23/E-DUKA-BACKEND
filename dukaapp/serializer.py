ab4960fca7c3d8ed344a2878f1e3d8d9a8f0210d
Josephshitandi <jsphshtnd@gmail.com>
2021-01-07 09:59:49 +0300

    Specified serializer field

--- dukaapp/serializer.py
+++ dukaapp/serializer.py
@@ -37,7 +37,7 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id','category','image','card']
         
 class Sub_CategorySerializer(serializers.ModelSerializer):
-    # category = serializers.CharField(source='category.category')
+    category = serializers.CharField(source='category.category')
     class Meta:
         model = Sub_Category
         fields = ['id','name','description','category']
