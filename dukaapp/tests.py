3cdf0052614f10ad63d5b1c8225c36e143f295ab
kipkoech-msojo <collinskipkoech95@gmail.com>
2021-01-08 14:36:12 +0300

    added tests code to test the category setup, instance and save method

--- dukaapp/tests.py
+++ dukaapp/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from .models import Category,Sub_Category,Shop,Product,Order,Comment,Profile
 
-# Create your tests here.
+
+class CategoryTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.category1= Category(category='electronics',image='avatar.png')
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category1,Category))
+
+    # Testing Save Method
+    def test_save_method(self):
+        self.category1.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
