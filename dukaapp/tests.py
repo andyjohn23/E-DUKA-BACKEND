3e6976c69e7bfa0f6883e55c80560dae65c0a928
kipkoech-msojo <collinskipkoech95@gmail.com>
2021-01-09 08:19:31 +0300

    wrote tests to test products model creation, instance, save method and delete method

--- dukaapp/tests.py
+++ dukaapp/tests.py
@@ -20,32 +20,44 @@ class CategoryTestClass(TestCase):
 
 class SubCategoryTestClass(TestCase):
     
-    # Set up method
+    
     def setUp(self):
         self.category1= Category.objects.create(category='electronics',image='avatar.png')
         self.sub_category1= Sub_Category.objects.create(name='laptops',description='brand new HP laptops',category=self.category1)
-    # Testing  instance
+    
     def test_instance(self):
         self.assertTrue(isinstance(self.sub_category1,Sub_Category))
 
-    # Testing Save Method
+    
     def test_save_sub_category(self):
         self.sub_category1.save_sub_category()
         sub_categories = Sub_Category.objects.all()
         self.assertTrue(len(sub_categories) > 0)
-
-# class ProfileTestClass(TestCase):
     
-#     # Set up method
-#     def setUp(self):
-#         self.profile= Profile(category='electronics',image='avatar.png')
-#     # Testing  instance
-#     def test_instance(self):
-#         self.assertTrue(isinstance(self.category1,Category))
+   
+    
+class ProductTestClass(TestCase):
+    
+    
+    def setUp(self):
+        self.category1= Category.objects.create(category='electronics',image='avatar.png')
+        self.sub_category1= Sub_Category.objects.create(name='laptops',description='brand new HP laptops',category=self.category1)
+        self.product1 = Product.objects.create(item_name='laptop',description='HP corei5, 2GB RAM',price=40000,image='avatar.png',sub_category=self.sub_category1)
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.product1,Product))
 
-#     # Testing Save Method
-#     def test_save_category(self):
-#         self.category1.save_category()
-#         categories = Category.objects.all()
-#         self.assertTrue(len(categories) > 0)
+    
+
+    def test_save_product(self):
+        self.product1.save_product()
+        products = Product.objects.all()
+        self.assertTrue(len(products) > 0)
+
+    def test_delete_product(self):
+        self.product1.delete_product()
+        products = Product.objects.all()
+        self.assertTrue(len(products) - 1)
+
+
 
