daf388afaae4e18a901ff2837e9aafd2b4951725
kipkoech-msojo <collinskipkoech95@gmail.com>
2021-01-09 08:04:59 +0300

    wrote tests to test sub category model

--- dukaapp/tests.py
+++ dukaapp/tests.py
@@ -6,14 +6,46 @@ class CategoryTestClass(TestCase):
     
     # Set up method
     def setUp(self):
-        self.category1= Category(category='electronics',image='avatar.png')
+        self.category1= Category.objects.create(category='electronics',image='avatar.png')
+        
     # Testing  instance
     def test_instance(self):
         self.assertTrue(isinstance(self.category1,Category))
 
     # Testing Save Method
-    def test_save_method(self):
+    def test_save_category(self):
         self.category1.save_category()
         categories = Category.objects.all()
         self.assertTrue(len(categories) > 0)
+
+class SubCategoryTestClass(TestCase):
+    
+    # Set up method
+    def setUp(self):
+        self.category1= Category.objects.create(category='electronics',image='avatar.png')
+        self.sub_category1= Sub_Category.objects.create(name='laptops',description='brand new HP laptops',category=self.category1)
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.sub_category1,Sub_Category))
+
+    # Testing Save Method
+    def test_save_sub_category(self):
+        self.sub_category1.save_sub_category()
+        sub_categories = Sub_Category.objects.all()
+        self.assertTrue(len(sub_categories) > 0)
+
+# class ProfileTestClass(TestCase):
+    
+#     # Set up method
+#     def setUp(self):
+#         self.profile= Profile(category='electronics',image='avatar.png')
+#     # Testing  instance
+#     def test_instance(self):
+#         self.assertTrue(isinstance(self.category1,Category))
+
+#     # Testing Save Method
+#     def test_save_category(self):
+#         self.category1.save_category()
+#         categories = Category.objects.all()
+#         self.assertTrue(len(categories) > 0)
 
