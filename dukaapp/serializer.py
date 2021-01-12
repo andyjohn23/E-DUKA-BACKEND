57a117bf0e224d366190cfb46d6009113dd37680
kipkoech-msojo <collinskipkoech95@gmail.com>
2021-01-12 11:11:28 +0300

    configure travis

--- dukaapp/serializer.py
+++ dukaapp/serializer.py
@@ -46,7 +46,7 @@ class ProductSerializer(serializers.ModelSerializer):
     # sub_category = serializers.CharField(source='sub_category.name')
     class Meta:
         model = Product
-        fields = ['id','item_name','description','price','date_added','sub_category','quantity','color','previous_price','shipped_from','size','brand','image','image2','image3','image4']
+        fields = ['id','item_name','description','price','date_added','sub_category','quantity','color','previous_price','size','brand','image','image1','image2','image3']
         
         
 class CommentSerializer(serializers.ModelSerializer):
