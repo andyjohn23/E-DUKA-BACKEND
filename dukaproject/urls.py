bfec307cddd90d7a47e13497b3000b2847214c25
kipkoech-msojo <collinskipkoech95@gmail.com>
2021-01-13 10:04:11 +0300

    fixing errot

--- dukaproject/urls.py
+++ dukaproject/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     path('tinymce/', include('tinymce.urls')),
     path('api-token-auth/', obtain_auth_token),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
